9aabbd7ac559547abdc53c01dd0c68aeeec83a8d
Arturo Gonzalez <arturogonzalez@Arturos-MacBook-Pro.local>
2016-10-04 11:24:49 +0200

    Modified the main file adding features to clean up the data coming from the input file.

--- main.py
+++ main.py
@@ -4,14 +4,32 @@ import csv
 Read CSV file
 """
 
+import csv
 
-def csv_reader(file_obj):
-    reader = csv.reader(file_obj)
-    for row in reader:
-        print(" ".join(row))
+csv.register_dialect(
+    'CSV_reader',
+    delimiter=',',
+    quotechar='"',
+    doublequote=True,
+    skipinitialspace=True,
+    lineterminator='\r\n',
+    quoting=csv.QUOTE_MINIMAL)
 
+with open('data/users.csv') as csvfile:
+    input_file = csv.reader(csvfile, dialect='CSV_reader')
+    next(input_file, None)
+    names = []
+    surnames = []
+    emails = []
+    for column in input_file:
+        name = column[0].replace('!', '').strip().title()
+        surname = column[1].replace('!', '').strip().title()
+        email = column[2].replace('!', '').strip().lower()
 
-if __name__ == "__main__":
-    csv_path = 'data/users.csv'
-    with open(csv_path) as f_obj:
-        csv_reader(f_obj)
+        names.append(name)
+        surnames.append(surname)
+        emails.append(email)
+
+        output = name + "\t \t" + surname + "\t \t" + email
+
+        print(output)
