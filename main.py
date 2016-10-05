8396b4cb5cb4c9c469ec52dee56d05157c974ca8
Rachel Chu <churac0@saig.frd.global>
2016-10-05 18:36:41 +1100

    using decorator

--- main.py
+++ main.py
@@ -11,17 +11,29 @@ csv.register_dialect(
     quoting=csv.QUOTE_MINIMAL)
 
 
+def print_csv(func):
+    def func_wrapper(file):
+        content = func(file)
+        for line in content:
+            print(line)
+
+    return func_wrapper
+
+
+@print_csv
 def csv_reader(file):
-    reader = csv.DictReader(file, dialect='CSV_reader')
-    word_char_pattern = re.compile(r'\w|@|\s+', re.IGNORECASE)
-    for line in reader:
-        print(''.join(c for c in
-                      line["name"].title() + "\t \t" +
-                      line["surname"].title() + "\t \t" +
-                      line["email"].lower()
-                      if bool(word_char_pattern.match(c))))
+    with open(file, 'r') as f:
+        reader = csv.DictReader(f, dialect='CSV_reader')
+        word_char_pattern = re.compile(r'\w|@|\s+', re.IGNORECASE)
+        clean_up = lambda x: ''.join([c for c in x["name"].title()
+                                      + "\t \t"
+                                      + x["surname"].title()
+                                      + "\t \t"
+                                      + x["email"].lower() if bool(word_char_pattern.match(c))])
+
+        content = list(map(clean_up, reader))
+        return content
 
 
 if __name__ == "__main__":
-    with open('data/users.csv') as f:
-        csv_reader(f)
+    csv_reader('data/users.csv')
